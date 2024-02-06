9cfcd0bca055df521e5d11380b155db0f3290412
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-02-06 20:14:57 +0100

    11th commit

--- ringoffire/src/app/app.config.ts
+++ ringoffire/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-db2d4","appId":"1:601326967533:web:cc00d8126e20d6bb648ae4","storageBucket":"ring-of-fire-db2d4.appspot.com","apiKey":"AIzaSyBohOYLQ-K3lN9S0eX0gqh2VWks2El5k1s","authDomain":"ring-of-fire-db2d4.firebaseapp.com","messagingSenderId":"601326967533"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -5,7 +5,7 @@ import { PlayerComponent } from '../player/player.component';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
 import {
   MatDialog,
   MAT_DIALOG_DATA,
@@ -23,6 +23,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { GameInfoComponent } from '../game-info/game-info.component';
 
+//import { FirestoreModule, getFirestore, provideFirestore,  } from '@angular/fire/firestore';
+
+import { Firestore, collection  } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-game',
   standalone: true,
@@ -34,7 +38,10 @@ import { GameInfoComponent } from '../game-info/game-info.component';
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
-  GameInfoComponent
+  GameInfoComponent,
+  //FirestoreModule,
+  
+  //Firestore,
   ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
@@ -45,11 +52,19 @@ export class GameComponent {
   currentCard: string = '';
   game!: Game;
 
+  //firestore: Firestore = inject(Firestore); <-- der Übeltäter
+
+ngOnInit(): void {
+  //this.firestore.collection('items').valueChanges().subscribe((game) => {
+   //console.log('Game Update', game);  <-- Hier auch
+  //});
+}
+
   newGame() {
     this.game = new Game();
   }
 
-  constructor(public dialog: MatDialog) {
+  constructor(private firestore: Firestore, public dialog: MatDialog) {
     this.newGame(); // Hier wird ein neues Spiel initialisiert. WICHTIG: Das war nötig, damit meine Funktion läuft, bevor... 
   } // die nächste Funktion geladen wird, die ja auf newGame() zugreifen will. Diese braucht man, um Game zu initialisiren, damit die pop()-Funktion die Daten von game.stack ablesen kann
 
